360cd4121a1e96f029bb48a0d4b4e2396010f936
cnloong <cnloong@connect.ust.hk>
2017-12-19 22:53:03 +0800

    Updated files by removing bugs

--- main_code.py
+++ main_code.py
@@ -202,6 +202,45 @@ for n in list_of_n:
         gc.collect()
 
 #%%
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the image file")
+args = vars(ap.parse_args())
+
+# load the image from disk
+image = cv2.imread(args["image"])
+ 
+# loop over the rotation angles
+for angle in np.arange(0, 360, 15):
+	rotated = imutils.rotate(image, angle)
+	cv2.imshow("Rotated (Problematic)", rotated)
+	cv2.waitKey(0)
+ 
+# loop over the rotation angles again, this time ensuring
+# no part of the image is cut off
+for angle in np.arange(0, 360, 15):
+	rotated = imutils.rotate_bound(image, angle)
+	cv2.imshow("Rotated (Correct)", rotated)
+	cv2.waitKey(0)
+
+image = np.asarray(  
+        [[0,0,0,1,0,0,0,0,0,0],
+         [0,0,1,1,0,0,0,1,1,0],
+         [0,0,0,1,0,0,1,0,0,1],
+         [0,0,0,1,0,0,0,1,1,1],
+         [0,0,0,1,0,0,0,0,0,1],
+         [0,0,0,1,0,0,0,0,0,1],
+         [0,0,0,0,0,1,0,1,0,1],
+         [0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0]])
+
+    
+#%%
 image = np.asarray(  
         [[0,0,0,1,0,0,0,0,0,0],
          [0,0,1,1,0,0,0,1,1,0],
